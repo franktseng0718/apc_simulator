7efcfaa4e74db7361235a44699e62cf1b5696964
buck5712 <104512304+buck5712@users.noreply.github.com>
2022-06-14 16:06:00 +0800

    Update process.js
    Add steak type

--- src/apcService/routers/v1/process.js
+++ src/apcService/routers/v1/process.js
@@ -32,7 +32,13 @@ router.post('/api/v1/process', async (req, res) => {
     let data = null;
     if (type === 'SHARON') {
       data = sharonStrategy(thickness, tFactor);
-    } else {
+    } else if (type === 'RIB_EYE'){
+      data = sharonStrategy(thickness, tFactor);
+    }
+      else if (type === 'NEW_YORK'){
+      data = sharonStrategy(thickness, tFactor);
+    }
+      else{
       data = defaultStrategy(moisture, mFactor);
     }
     axios({
